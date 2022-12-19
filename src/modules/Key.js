cef62bbaa5535d84e3716f15f5c6d401e5c5ddff
Rafael Ramos <ingrnrg@gmail.com>
2022-12-19 01:04:24 -0600

    added key.js

--- src/modules/Key.js
+++ src/modules/Key.js
@@ -1,14 +1,10 @@
 import { DataTypes } from 'sequelize'
 import { sequelize } from '../database/database.js'
 
-export const Album = sequelize.define('album', {
-    id_album: {
+export const Key = sequelize.define('key',{
+    id_key: {
         type: DataTypes.INTEGER,
         autoIncrement: true,
         primaryKey: true
-    },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false
     }
 })
